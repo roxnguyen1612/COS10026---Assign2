8866421e5ace37942d83947d998f4e13c64de338
gianni35011 <g35011-accounts@protonmail.com>
2022-05-28 04:01:59 +1000

    Fixed phpEnhancements css

--- phpEnhancements.php
+++ phpEnhancements.php
@@ -25,7 +25,7 @@ delete this after you read
   <section id="enhancement1">
     <div class="enhancement1 econtainer">
       <div class="enhancement1-top">
-        <h1 class="section-title"><span>IMPROVEMENT BRIEF</span></h1>
+        <h1 class="section-title">PHP <span>IMPROVEMENTS</span></h1>
         <p> Things we enhance on this assignment</p>
       </div>
     </div>
@@ -59,15 +59,14 @@ delete this after you read
   <section id="enhancement2">
     <div class="enhancement2 econtainer">
       <div class="enhancement2-top">
-        <h1 class="section-title"><span>ENHANCEMENTS DETAILS</span></h1>
+        <h1 class="section-title">ENHANCEMENT <span>DETAILS</span></h1>
         <p>To provide better understanding, we will provide a showcase of code and brief explanation.</p>
       </div>
-
       <!-- markquiz enh_1 -->
-      <div class="all-contents">
+      <div class="all-contents mt-5 mb-5">
         <div class="enhancement2-items">
           <div class="enhancement2-info">
-            <h2>session_start()</h2>
+            <h2>Using sessions to keep track of attempts</h2>
             <ul>
               <li>Give student maximum 2 attempts</li>
               <li>Store in $_SESSION["test_atmpt"]</li>
@@ -78,20 +77,36 @@ delete this after you read
             <img src="./Images/mq-1.png" alt="img" />
           </div>
         </div>
-      </div>
-      <!-- mquiz showcase -->
-      <div>
-        <p>When attempt = 1:</p>
-        <img src="./Images/mq-2.png" alt="img" />
-        <p>When attempt = 2:</p>
-        <img src="./Images/mq-3.png" alt="img" />
-      </div>
-
-      <!-- login page -->
-      <div class="all-contents">
         <div class="enhancement2-items">
           <div class="enhancement2-info">
-            <h2>Login session_start()</h2>
+            <h2>Attempt 1</h2>
+            <ul>
+              <li>After the first attempt, the student can decide to retry the quiz.</li>
+            </ul>
+          </div>
+          <div class="enhancement2-img">
+          <img src="./Images/mq-2.png" alt="img" />
+          </div>
+        </div>
+        <div class="enhancement2-items">
+          <div class="enhancement2-info">
+            <h2>Attempt 2</h2>
+            <ul>
+              <li>After the second attempt, the retry button is disabled.</li>
+              <li>The only option is to go back to the home page.</li>
+              </ul>
+          </div>
+          <div class="enhancement2-img">
+            <img src="./Images/mq-3.png" alt="img" />
+          </div>
+        </div>
+      </div>
+      <!-- mquiz showcase -->
+      <!-- login page -->
+      <div class="all-contents mt-5 mb-5">
+        <div class="enhancement2-items">
+          <div class="enhancement2-info">
+            <h2>Using sessions to keep track of login attempts and status.</h2>
             <ul>
               <li>Store username and password information</li>
               <li>$_POST["hidden"] input to calculate fail times</li>
@@ -101,39 +116,92 @@ delete this after you read
           <div class="enhancement2-img">
             <img src="./Images/lg-1.png" alt="img" />
           </div>
-          <br>
-          <div>
-            <p>Checking login in manage.php:</p>
+        </div>
+        <div class="enhancement2-items">
+          <div class="enhancement2-info">
+            <h2>Checking user login within manage.php</h2>
+            <ul>
+              <li>If neither the 'user' or 'pwd' are set</li>
+              <li>the user will be sent back to the login page.</li>
+            </ul>
+          </div>
+          <div class="enhancement2-img">
             <img src="./Images/lg-2.png" alt="img" />
-            <p>$_POST["hidden"] > 3 = disable button</p>
+          </div>
+        </div>
+        <div class="enhancement2-items">
+          <div class="enhancement2-info">
+            <h2>Checking login attempts</h2>
+            <ul>
+              <li>When a user has attempted to login more than 3 times the login button will be disabled.</li>
+            </ul>
+          </div>
+          <div class="enhancement2-img">
             <img src="./Images/lg-3.png" alt="img" />
+          </div>
+        </div>
+        <div class="enhancement2-items">
+          <div class="enhancement2-info">
+            <h2>Disabling Login Button</h2>
+            <ul>
+              <li>If $atmpt is equal to three, the login button will be disabled.</li>
+            </ul>
+          </div>
+          <div class="enhancement2-img">
             <img src="./Images/lg-4.png" alt="img" />
           </div>
         </div>
       </div>
 
       <!-- database -->
-      <div class="all-contents">
+      <div class="all-contents mt-5 mb-5">
         <div class="enhancement2-items">
           <div class="enhancement2-info">
             <h2>Database</h2>
             <ul>
-            <li>Store answers & calculate score</li>
-            <li>Record students' attempts</li>
-            <li>Output 5 functions in manage page</li>
+              <li>Store correct answers</li>
+              <li>Record students' attempts</li>
+              <li>Output 5 functions in manage page</li>
             </ul>
           </div>
           <div class="enhancement2-img">
             <img src="./Images/img-1.png" alt="img" />
           </div>
         </div>
-        <div>
-          <p>Calculate score on database answers: </p>
-          <img src="./Images/db-1.png" alt="img" />
-          <p>Insert student attempts: </p>
-          <img src="./Images/db-2.png" alt="img" />
-          <p>Functions in manage.php by displaytable.php. For example:  </p>
-          <img src="./Images/db-3.png" alt="img" />
+        <div class="enhancement2-items">
+          <div class="enhancement2-info">
+            <h2>Calculate scores and stored in our database</h2>
+            <ul>
+              <li>markquiz.php calculates student scores.</li>
+              <li>The correct answers, which are stored in our database are used to calculate the score.</li>
+            </ul>
+          </div>
+          <div class="enhancement2-img">
+            <img src="./Images/db-1.png" alt="img" />
+          </div>
+        </div>
+        <div class="enhancement2-items">
+          <div class="enhancement2-info">
+            <h2>Insert student attempts</h2>
+            <ul>
+              <li>Once the score has been calcuated: student information, attempt number, and score are all stored in our database.</li>
+            </ul>
+          </div>
+          <div class="enhancement2-img">
+            <img src="./Images/db-2.png" alt="img" />
+          </div>
+        </div>
+        <div class="enhancement2-items">
+          <div class="enhancement2-info">
+            <h2>manage.php functions</h2>
+            <ul>
+              <li>There are 5 functions within our manage.php page that allow supervisors to manage the database.</li>
+              <li>These functions allow the supervisor to delete attempts, list attempts, and change the scores of students.</li>
+            </ul>
+          </div>
+          <div class="enhancement2-img">
+            <img src="./Images/db-3.png" alt="img" />
+          </div>
         </div>
       </div>
     </div>
@@ -141,7 +209,7 @@ delete this after you read
 
   <!-- reference -->
   <section>
-  <div id="ref">
+    <div id="ref">
       <h2 id="hd">Reference:</h2>
       <ol>
         <li>Nurullah, Md. <em>Display Data in an HTML Table Using PHP & MySQL</em>, <a href="https://codingstatus.com/display-data-in-html-table-using-php-mysql/">https://codingstatus.com/display-data-in-html-table-using-php-mysql/</a></li>
